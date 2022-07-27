dd2971c4272e304640c122935575ae59e4e9ab95
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2022-07-27 08:40:19 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -104,10 +104,7 @@ precompute:
     }
 */
 ll ncr(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-ll npr(ll n, ll r, ll m, ll *fact, ll *ifact) {
-    ll val1 = fact[n]; ll val2 = ifact[n - r];
-    return (((val1 * val2) % m));
-}
+ll npr(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r];return (((val1 * val2) % m));}
 
 void google(int t) {cout << "Case #" << t << ": ";}
 vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
