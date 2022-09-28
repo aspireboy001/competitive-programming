9f116b90be1c7d693cdba6e357268da3af42ec4d
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2022-09-29 00:45:39 +0530

    Create Sieve + Prime_Factors_with_Power.cpp

---
+++ math/Sieve + Prime_Factors_with_Power.cpp
@@ -0,0 +1,46 @@
+
+
+vll primes(1e6 + 1, 1) ;
+vll prime;
+
+void sieve() {
+
+    primes[0] = primes[1] = 0 ;
+
+    for (int i = 2; i * i <= 1e6; i++) {
+
+        if (primes[i]) {
+            prime.push_back(i);
+            for (int j = i * i ; j <= 1e6 ; j += i)
+                primes[j] = 0;
+        }
+
+    }
+
+    for (int i = 1e3 + 1; i <= 1e6; i++) {
+        if (primes[i])
+            prime.push_back(i);
+    }
+}
+
+vpll getPrimeFactorsWithPower(ll n) {
+
+    vpll v ;
+
+    ll nn = n ;
+
+    for (int i = 0 ; i < prime.size() and prime[i]*prime[i] <= nn ; i++) {
+
+        int ct = 0 ;
+
+        while (n % prime[i] == 0) {
+            n /= prime[i];
+            ct++;
+        }
+        if (ct > 0) v.pb({prime[i], ct});
+    }
+
+    if (n > 1) v.pb({n, 1});
+
+    return v ;
+}
