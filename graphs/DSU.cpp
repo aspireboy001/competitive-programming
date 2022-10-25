457940c863badd301b2c5b9d12720f967d2f5e93
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2022-10-25 16:07:22 +0530

    Update DSU.cpp

--- graphs/DSU.cpp
+++ graphs/DSU.cpp
@@ -1,33 +1,34 @@
-//Union Find Algorithm Almost O(1) for reasonable size N. O(log*N) to be precise
-struct UnionFind {
-	int n;
-	vector<int> rank;
-	vector<int> parent;
-	// store other info as required
-	UnionFind(int n) {
-		rank.resize(n);
-		fill(rank.begin(), rank.end(), 0);
-		parent.resize(n);
-		for (int i = 0; i < n; i++) {
-			parent[i] = i;
-		}
-	}
-	int get(int a) {
-		return parent[a] = (parent[a] == a ? a : get(parent[a]));
-	}
-	void merge(int a, int b) {
-		a = get(a);
-		b = get(b);
-		if (a == b) {
-			return;
-		}
-		if (rank[a] == rank[b]) {
-			rank[a]++;
-		}
-		if (rank[a] > rank[b]) {
-			parent[b] = a;
-		} else {
-			parent[a] = b;
-		}
-	}
+class DSU {
+
+public:
+
+    vector<int> parent, size;
+
+    DSU(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.push_back(i);
+            size.push_back(1);
+        }
+    }
+
+    int findPar(int node) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void unionS(int u, int v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+        if (pu == pv)
+            return;
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
 };
