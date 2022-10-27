f60a5e3e4935a7cac83d310d96f2bd563dd2d7c0
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2022-10-27 15:24:01 +0530

    Create mini_template.cpp

---
+++ mini_template.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define nline "\n"
+#define pb push_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pair<ll,ll>>
+#define vvll vector<vector<ll>>
+typedef long long ll;
+typedef long double lld;
+
+
+void solve() {
+
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int test_cases = 1 ;
+    cin >> test_cases ;
+
+    while (test_cases--) {
+        solve();
+    }
+
+}
