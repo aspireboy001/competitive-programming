bca9f144b20588264114d6d4610b2ebdb45c6ccd
aspireboy001 <97575801+aspireboy001@users.noreply.github.com>
2022-11-23 09:19:35 +0530

    Create mini_template.cpp

---
+++ templates/mini_template.cpp
@@ -0,0 +1,59 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define inf 1e18
+#define iinf INT_MIN
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define parity_check __builtin_parity //1=odd,0=even
+#define set_bits __builtin_popcountll
+#define leading_zeros __builtin_clzll //00010 = 3
+#define trailing_zeros __builtin_ctzll //00010 = 1
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vii vector<int>
+#define pll pair<ll,ll>
+#define vpii vector<pair<int,int>>
+#define vll vector<ll>
+#define eb emplace_back
+#define vpll vector<pair<ll,ll>>
+#define vch vector<char>
+#define vvll vector<vector<ll>>
+#define min_heap priority_queue <int, vector<int>, greater<int>>
+#define max_heap priority_queue <int>
+#define rep(var , start , end) for(int var = start ; var < end ; var++)
+#define rrep(var , start , end) for(int var = start ; var >= end ; var--)
+#define py cout<<"YES"<<nline
+#define pn cout<<"NO"<<nline
+
+typedef long long ll;
+typedef long double lld;
+
+
+void solve() {
+
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int test_cases = 1 ;
+    cin >> test_cases ;
+
+    while (test_cases--) {
+        solve();
+    }
+
+}
